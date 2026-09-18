975eec56f073758d716d6e9647968074329e785f
Ahmer <27773197+wpcoder110@users.noreply.github.com>
2026-09-18 08:19:04 +0000

    chore: release v0.1.1

--- packages/payload-plugin/src/rsc.tsx
+++ packages/payload-plugin/src/rsc.tsx
@@ -459,7 +459,7 @@ export function BlockwrightNavLink({ payload }: { payload?: BasePayload }) {
   )
 }
 
-const PKG_VERSION = '0.1.0'
+const PKG_VERSION = '0.1.1'
 
 export async function BlockwrightOverviewView({ initPageResult }: EditorViewProps) {
   const req = initPageResult?.req
